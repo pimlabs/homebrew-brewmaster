ab7e9845d51ad3051aba31ca7758aace696db030
Eko Dedy Purnomo <eko.dedy.purnomo@gmail.com>
2026-06-09 09:53:14 +0700

    feat: add brewmaster formula v0.3.0

---
+++ Formula/brewmaster.rb
@@ -0,0 +1,23 @@
+class Brewmaster < Formula
+  desc "Selective Homebrew package upgrades by semver bump level"
+  homepage "https://github.com/pimlabs/brewmaster"
+  url "https://github.com/pimlabs/brewmaster/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "7e1a1887373bb397025fa8eb2d95016c6b0a3efb9733056088bc5a2eed677282"
+  version "0.3.0"
+  license "MIT"
+
+  depends_on "jq"
+
+  def install
+    bin.install "bin/brewmaster"
+    libexec.install Dir["lib/brewmaster"]
+    # Rewrite LIB_DIR to point to the installed location
+    inreplace bin/"brewmaster",
+      'LIB_DIR="${BREWMASTER_LIB:-$SCRIPT_DIR/../lib/brewmaster}"',
+      "LIB_DIR=\"${BREWMASTER_LIB:-#{libexec}}\""
+  end
+
+  test do
+    assert_match "brewmaster 0.3.0", shell_output("#{bin}/brewmaster --version")
+  end
+end
